39403dedb79978fa23c0fa02ae9c4d78b3c80370
Daiana Oliveira <daiana.davi2411@gmail.com>
2022-02-08 17:17:13 -0300

    aula remota 3 footer consta com erro na galeria de fotos e no footer

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mostrarPopup(){
+    window.alert("wello Word")
+}
+
+let email = document.getElementById("campo-email")
+
+function enviarEmail(){
+    let emailDigitado = email.Value;
+    console.log(emailDigitado)
+}
