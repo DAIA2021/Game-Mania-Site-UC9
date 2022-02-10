06601102e90d514239c1f71746aab1e2208ad6c7
Daiana Oliveira <daiana.davi2411@gmail.com>
2022-02-10 16:54:41 -0300

    rodapé e carrossel com erro

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-function mostrarPopup(){
-    window.alert("wello Word")
-}
+// function mostrarPopup(){
+//     window.alert("wello Word")
+// }
+
+
 
 let email = document.getElementById("campo-email")
 
@@ -8,3 +10,49 @@ function enviarEmail(){
     let emailDigitado = email.Value;
     console.log(emailDigitado)
 }
+
+// jquery...
+
+$(document).ready(function(){
+
+   $("#barras").click(function(){
+       if($("#menu").hasClass("#menu-ativo")){
+           $("#menu").removeClass("#menu-ativo")
+       }else{
+           $("#menu").addClass("menu-ativo")
+       }
+   })
+     
+})
+    
+//    {) fechamento da função abertura ( antes da function
+
+// $(document).ready(function(){
+
+//     $("#barras").click(function(){
+//         $("#menu").toggleClass("#menu-ativo") 
+
+// })
+// })
+
+
+// criação função carrosel
+
+$(document).ready(function(){
+   $("carrosel img:eq(0)").addClass("banner-ativo").show()
+
+})
+
+setInterval(slide,1000)
+function slide(){
+    
+    if($(".banner-ativo").next().length){
+        $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+    }else{
+        $(".banner-ativo").removeClass().hide()
+        $("carrosel img:eq(0)").addClass("banner-ativo").show()
+
+    }
+}
+
+   
