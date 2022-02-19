e3456d4d154ff5d047b837be3d1f30fadb313fe1
Daiana Oliveira <daiana.davi2411@gmail.com>
2022-02-18 23:00:42 -0300

    realizado carrosel banner

--- js/script.js
+++ js/script.js
@@ -1,76 +1,70 @@
 // function mostrarPopup(){
-//     window.alert("wello Word")
-// }
-
-
-
-let email = document.getElementById("campo-email")
-
-function enviarEmail(){
-    let emailDigitado = email.Value;
-    console.log(emailDigitado)
-}
-
-// jquery...
-
- $(document).ready(function(){
-
-    $("#barras").click(function(){
-       if($("#menu").hasClass("#menu-ativo")){
-         $("#menu").removeClass("#menu-ativo")
-       }else{
-           $("#menu").addClass("menu-ativo")
-       }
-    })
-     
- })
- 
+    //     window.alert("wello Word")
+    // }
     
-//    {) fechamento da função abertura ( antes da function
-
- //$(document).ready(function(){
-
- //   $("#barras").click(function(){
- //       $("#menu").toggleClass("#menu-ativo") 
-
-//})
-//})
-
-
-// criação função carrosel
-
-$(document).ready(function(){
-   $("carrosel img:eq(0)").addClass("banner-ativo").show()
-
-})
-
-setInterval(slide,1000)
-
-function slide(){
+    $(document).ready(function(){
     
-    if($(".banner-ativo").next().length){
-        $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
-    }else{
-        $(".banner-ativo").removeClass().hide()
-        $("carrosel img:eq(0)").addClass("banner-ativo").show()
-
+        $("#barras").click(function(){
+           if($("#menu").hasClass("#menu-ativo")){
+             $("#menu").removeClass("#menu-ativo")
+           }else{
+               $("#menu").addClass("menu-ativo")
+           }
+        })
+    
+        $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+    
+        setInterval(slide, 1000)
+    
+        function slide(){
+            if($(".banner-ativo").next().length ){
+                $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+            }else{
+                $(".banner-ativo").removeClass().hide()
+                $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+            }
+        }
+         
+     })
+    
+    let email = document.getElementById("campo-email")
+    
+    function enviarEmail(){
+        let emailDigitado = email.Value;
+        console.log(emailDigitado)
     }
-}
-
-
-//login funcionalidades
-
-let email= document.getElementById("campo-email");
-
-
-function enviarEmail(){
-    let emailDigitado = email.Value;
-    console.log (emailDigitado)
-}
-   
-let senha= document.getElementById("campo-senha");
-
-function enviarSenha(){
-    let senhaDigitado = senha.valeu;
-    console.log(senhaDigitado)
-}
+    
+    // jquery...
+    
+     
+     
+        
+    //    {) fechamento da função abertura ( antes da function
+    
+     //$(document).ready(function(){
+    
+     //   $("#barras").click(function(){
+     //       $("#menu").toggleClass("#menu-ativo") 
+    
+    //})
+    //})
+    
+    
+    // criação função carrosel
+    
+    //login funcionalidades
+    
+    let emaillogin = document.getElementById("campo-email");
+    
+    
+    function enviarEmail(){
+        let emailDigitado = emaillogin.Value;
+        console.log (emailDigitado)
+    }
+       
+    let senhalogin = document.getElementById("campo-senha");
+    
+    function enviarSenha(){
+        let senhaDigitado = senhalogin.valeu;
+        console.log(senhaDigitado)
+    }
